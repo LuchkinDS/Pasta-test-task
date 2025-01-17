e62f7a12cc28af3da9177c3e99457fb4407c6c93
LuchkinDS <LuchkinDS@gmail.com>
2025-01-17 15:58:32 +0700

    fix test

--- app/tests/WebTests/ShowPublicPasteTest.php
+++ app/tests/WebTests/ShowPublicPasteTest.php
@@ -2,29 +2,61 @@
 
 namespace App\Tests\WebTests;
 
-use Doctrine\ORM\Tools\SchemaTool;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class ShowPublicPasteTest extends WebTestCase
 {
-    protected function setUp(): void
-    {
-        $kernel = self::bootKernel();
-
-        $this->entityManager = $kernel->getContainer()
-            ->get('doctrine')
-            ->getManager();
-
-        $schemaTool = new SchemaTool($this->entityManager);
-        $metadata = $this->entityManager->getMetadataFactory()->getAllMetadata();
-
-        $schemaTool->dropSchema($metadata);
-        $schemaTool->createSchema($metadata);
-        parent::setUp();
-    }
-
-    public function testShowPublicPaste(): void
+    public function testShowForm(): void
     {
         $client = static::createClient();
+        static::bootKernel();
+
+        $client->request('GET', '/paste/create');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorExists('form');
+    }
+
+    public function testUnCorrectFillingForm(): void
+    {
+        $client = static::createClient();
+        static::bootKernel();
+
+        $crawler = $client->request('GET', '/paste/create');
+
+        $form = $crawler->selectButton('Cooking')->form();
+        $client->submit($form);
+
+        $this->assertResponseIsUnprocessable();
+        $this->assertSelectorTextContains('div>ul>li', 'This value should not be blank.');
+    }
+
+    public function testCorrectFillingForm(): void
+    {
+        $client = static::createClient();
+        static::bootKernel();
+
+        $crawler = $client->request('GET', '/paste/create');
+        $form = $crawler->selectButton('Cooking')->form();
+        $form['paste[title]']->setValue('Paste test title');
+        $form['paste[content]']->setValue('Paste test content');
+
+        $client->submit($form);
+
+        $this->assertResponseRedirects();
+    }
+
+    public function testSubmitPasteWithoutTitle(): void
+    {
+        $client = static::createClient();
+        static::bootKernel();
+
+        $crawler = $client->request('GET', '/paste/create');
+        $form = $crawler->selectButton('Cooking')->form();
+        $form['paste[content]']->setValue('Paste test content');
+
+        $client->submit($form);
+
+        $this->assertResponseRedirects();
     }
 }
